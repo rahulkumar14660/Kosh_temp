344135b5dc36438a55ae1828e3ea78872a061f4a
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-10-29 23:43:51 +0530

    added database into backend

---
+++ server/database/db.js
@@ -0,0 +1,1 @@
+import mongoose from "mongoose";export const connectDB = async () => {  try {    return await mongoose.connect(`${process.env.MONGO_URI}`, {      dbName: "LIBRARY",    });  } catch (err) {    console.log("Error connecting to database", err);  }};
